fac49222347134f78b28b548d9782a109d8343c7
Eric Knecht <eric@ericknecht.com>
2013-02-27 10:35:00 -0500

    allow reporting per workflow type

--- amazon_swf_zombie/amazon_swf_zombie.rb
+++ amazon_swf_zombie/amazon_swf_zombie.rb
@@ -16,28 +16,25 @@ class AmazonSWFZombie < Scout::Plugin
     swf_domain:
       name: SWF Domain
       notes: Simple Workflow domain
-    interval_rescan:
-      name: Interval rescan
-      default: 3600
-      notes: Additional interval scanned before @last_run (sec) to check for late updates.  Defaults to one hour.
+    workflow_types:
+      name: Workflow Types
+      notes: Whitespace seperated list of workflows to check for zombies
     max_run_time:
       name: Max Run Time
       default: 1800
-      notes: We will search for workflows that are running for more than @max_run_time. Defaults to 30m. 
+      notes: We will search for workflows that are running for more than @max_run_time. Defaults to 30m.
   EOS
-  
+
   def init
     @access_key = option('access_key') or return error('access_key cannot be empty')
     @secret_key = option('secret_key') or return error('secret_key cannot be empty')
     @swf_domain = option('swf_domain') or return error('swf_domain cannot be empty')
     @max_run_time = option('max_run_time').to_i or return error('max_run_time cannot be empty')
-
-    # SWF results are eventually consistent.  interval_rescan is a means to
-    # recheck for late updates.
-    @interval_rescan = option('interval_rescan').to_i
+    @workflow_types = option('workflow_types') or return error('workflow_types cannot be empty')
+    @workflow_types = @workflow_types.split
     nil
   end
-  
+
   # Scout method
   def build_report
     return if init
@@ -51,16 +48,18 @@ class AmazonSWFZombie < Scout::Plugin
     swf = AWS::SimpleWorkflow.new
     domain = swf.domains[@swf_domain]
 
-    zombie_workflows = 0
-    total_workflows=0
+    zombie_workflows = {}
     domain.workflow_executions.each(:status => :open) do |execution|
-      unless execution.workflow_id.start_with? 'Cron'
-        zombie_workflows +=1 if (Time.now.to_i - execution.started_at.to_i) > @max_run_time
-        total_workflows +=1
+      name = execution.workflow_type.name
+      next unless @workflow_types.include? name
+      zombie_workflows[name.to_sym] = 0 unless zombie_workflows[name.to_sym]
+      if (Time.now.to_i - execution.started_at.to_i) > @max_run_time
+        zombie_workflows[name.to_sym] +=1
       end
     end
 
-    report( :zombie_workflows => zombie_workflows )
-    report( :total_workflows => total_workflows )
+    zombie_workflows.keys.each do |key|
+      report key => zombie_workflows[key]
+    end
   end
 end
